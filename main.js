4f37a132a0b7eca09466dea4a61fc9cb430b116d
GreatWhiteShork <48199571+GreatWhiteShork@users.noreply.github.com>
2021-06-17 19:52:56 +1000

    Improving phone experience
    Trying to full-screen the app on the phone. The address bar, scrolling, zooming and double-tap are all awful.

--- main.js
+++ main.js
@@ -25,6 +25,21 @@ var permStoneInfo = {
       knowledge: 3
 }
 
+function toggleFullScreen() {
+  var doc = window.document;
+  var docEl = doc.documentElement;
+
+  var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+  var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+  if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+    requestFullScreen.call(docEl);
+  }
+  else {
+    cancelFullScreen.call(doc);
+  }
+}
+
 var anim = lottie.loadAnimation({
 			container: document.getElementById("lottie"),
 			renderer: "svg",

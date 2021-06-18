1345b314ee086beb5782a7dd51d230324d99c55d
GreatWhiteShork <48199571+GreatWhiteShork@users.noreply.github.com>
2021-06-18 10:03:09 +1000

    Updating full-screen settings

--- main.js
+++ main.js
@@ -25,21 +25,9 @@ var permStoneInfo = {
       knowledge: 3
 }
 
-window.addEventListener("load", function() { window.scrollTo(0,1); } );
+window.addEventListener("load", function() { window.scrollTo(0,0); } );
 //document.addEventListener("touchmove", function(e) { e.preventDefault() } );
 
-  var doc = window.document;
-  var docEl = doc.documentElement;
-
-  var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
-  var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
-
-  if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
-    requestFullScreen.call(docEl);
-  }
-  else {
-    cancelFullScreen.call(doc);
-  }
 
 
 var anim = lottie.loadAnimation({
